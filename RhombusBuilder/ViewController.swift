a3b7b84a6e3b5b9bb3f348e8833f874a30beaf3c
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-02 15:04:08 +0300

    Initial Commit

---
+++ RhombusBuilder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RhombusBuilder
+//
+//  Created by Артем on 5/2/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
